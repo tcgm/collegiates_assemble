6b1c906e4d0076c73aa137f6c2cf8f26dfaeaa85
TCGM <thecrazygamemaster@gmail.com>
2016-10-18 23:54:08 -0700

    Removed currentValue. Changed userMenu to display the byte array. enterValue now zeroes out the rest of the array after reading input.

--- main.c
+++ main.c
@@ -1,20 +1,23 @@
 #include "stdio.h"
 
-int userMenu(int userSelection, int*);
+int userMenu(int *userSelection, int precision, unsigned char *bArray);
 
 int settings(int);
+
+void enterValue(char bArray[], int);
 
 int main(int argc, const char* argv[])
 {
 	int userSelection = 0;
-	int currentValue = 0;
-	int precision = 1;
+	int precision = 5;
 
-	unsigned char bArray[255] = { 0 };
+	unsigned char bArray[200] = { 0 };
 
-	userSelection = userMenu(userSelection, &currentValue, &bArray);
+	enterValue(&bArray, precision);
+	userSelection = userMenu(&userSelection, precision, &bArray);
+	
 
-	switch (userSelection){
+	switch (userSelection) {
 	case 1:
 		//asm func add
 		break;
@@ -30,32 +33,30 @@ int main(int argc, const char* argv[])
 	case 5:
 		precision = settings(precision);
 		break;
-	case 6: 
+	case 6:
 		exit(1);
 		break;
 	}
 
-	for (int i = 0; i < 255; i++) {
-		printf("%d, ", bArray[i]);
-	}
-
-	printf("%d\n", currentValue);
+	//printf("%d\n", currentValue);
 
 	return 0;
 }
 
-int userMenu(int userSelection, int *currentValue, unsigned char *bArray)
+int userMenu(int *userSelection, int precision, unsigned char *bArray)
 {
-	printf("Enter a value: \n");
-	scanf("%d", bArray);
+	printf("Current Value: ");
 
+	for (int i = 0; i < precision; i++) {
+		printf("%d", bArray[i]);
+	}
 
-	printf("1. Add\n");
+	printf("\n1. Add\n");
 	printf("2. Sub\n");
 	printf("3. Mul\n");
 	printf("4. Div\n");
 	printf("5. Settings\n");
-	printf("6. Exit\n"); 
+	printf("6. Exit\n");
 	scanf("%d", &userSelection);
 	return userSelection;
 }
@@ -65,4 +66,22 @@ int settings(int precision)
 	printf("Enter precision: \n");
 	scanf("%d", &precision);
 	return precision;
+}
+
+void enterValue(unsigned char *bArray, int precision)
+{
+	//Prompt user to enter a value.
+	printf("Enter a value: \n");
+
+	//Read into the byte array their input.
+	for (int i = 0; i < precision; i++)
+	{
+		bArray[i] = getchar() - '0';
+		//bArray[i] = bArray[i] - '0';
+	}
+
+	//Insure that all other indexes of the array are 0.
+	for (int i = precision; i < 200; i++) {
+		bArray[i] = 0;
+	}
 }
